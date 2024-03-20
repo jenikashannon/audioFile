0614c82be88922f2aba34ccba867684c6f1b336d
Jenika Shannon <jenikashannon@pm.me>
2024-03-20 19:21:45 -0400

    add edit icon to crate items

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,7 +3,7 @@ import "./Item.scss";
 // libraries
 import { Link } from "react-router-dom";
 
-function Item({ item, type }) {
+function Item({ item, type, mode }) {
 	const user_id = localStorage.getItem("audioFileId");
 
 	let albumCount;
@@ -19,8 +19,21 @@ function Item({ item, type }) {
 		<Link to={`/crates/${user_id}/${item.id}`} className='item'>
 			<img src={item.cover_art} className='item__icon' />
 			<div className='item__container'>
-				<p className='item__name'>{item.name}</p>
-				{<p className='item__details'>{albumCount}</p>}
+				<div className='item__container--text'>
+					<p className='item__name'>{item.name}</p>
+					{<p className='item__details'>{albumCount}</p>}
+				</div>
+				{type === "crate" ? (
+					<svg
+						className='item__icon'
+						xmlns='https://localhost:1700/icons/edit.svg'
+						height='18'
+						viewBox='0 -960 960 960'
+						width='18'
+					>
+						<path d='M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z' />
+					</svg>
+				) : null}
 			</div>
 		</Link>
 	);
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -4,6 +4,7 @@ import Item from "../Item/Item";
 function ItemList({ crateList }) {
 	let itemList;
 	let type;
+	let mode;
 
 	if (crateList) {
 		itemList = crateList;
@@ -13,7 +14,7 @@ function ItemList({ crateList }) {
 	return (
 		<section className='item-list'>
 			{itemList.map((item) => {
-				return <Item key={item.id} item={item} type={type} />;
+				return <Item key={item.id} item={item} type={type} mode={mode} />;
 			})}
 		</section>
 	);
