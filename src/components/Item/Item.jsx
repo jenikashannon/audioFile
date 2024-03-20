159c7f64e39600158a514cac7ff8098d27d91d10
Jenika Shannon <jenikashannon@pm.me>
2024-03-20 17:31:09 -0400

    add basic styling

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,18 +1,26 @@
 import "./Item.scss";
-import { baseUrl } from "../../utils/consts";
 
 // libraries
 import { Link } from "react-router-dom";
 
 function Item({ item, type }) {
 	const user_id = localStorage.getItem("audioFileId");
-	console.log(item);
+
+	let albumCount;
+
+	if (type === "crate") {
+		albumCount =
+			item.album_count === 1
+				? item.album_count + " album"
+				: item.album_count + " albums";
+	}
+
 	return (
-		<Link to={`/crates/${user_id}/${item.id}`}>
-			<img src={item.cover_art} />
-			<div>
-				<p>{item.name}</p>
-				<p>{item.album_count}</p>
+		<Link to={`/crates/${user_id}/${item.id}`} className='item'>
+			<img src={item.cover_art} className='item__icon' />
+			<div className='item__container'>
+				<p className='item__name'>{item.name}</p>
+				{<p className='item__details'>{albumCount}</p>}
 			</div>
 		</Link>
 	);
