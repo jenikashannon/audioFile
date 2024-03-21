48afd55b686eb2ebca750c215555868b27c6fa30
Jenika Shannon <jenikashannon@pm.me>
2024-03-20 20:31:44 -0400

    connect form to create crate endpoint

--- src/pages/AddCratePage/AddCratePage.jsx
+++ src/pages/AddCratePage/AddCratePage.jsx
@@ -1,7 +1,53 @@
 import "./AddCratePage.scss";
+import { baseUrl } from "../../utils/consts";
+
+// libraries
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function AddCratePage() {
-	return <>add me, daddy</>;
+	const [crateName, setCrateName] = useState("new crate");
+
+	const user_id = localStorage.getItem("audioFileId");
+	const navigate = useNavigate();
+
+	function handleChange(event) {
+		setCrateName(event.target.value);
+	}
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+		try {
+			await axios.post(`${baseUrl}/crates/${user_id}`, {
+				name: event.target.crate_name.value,
+				user_id,
+			});
+
+			navigate("/");
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<main>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='crate_name'>name your crate</label>
+				<input
+					type='text'
+					value={crateName}
+					name='crate_name'
+					onChange={handleChange}
+				/>
+				<input
+					type='submit'
+					value='make my crate!
+                '
+				/>
+			</form>
+		</main>
+	);
 }
 
 export default AddCratePage;
