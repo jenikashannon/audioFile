795ef28b510e9f08d3e9e698fd20af7d142d61b7
Jenika Shannon <jenikashannon@pm.me>
2024-03-29 12:11:01 -0400

    refine Spotify auth flow on UsersPage

--- src/pages/UsersPage/UsersPage.jsx
+++ src/pages/UsersPage/UsersPage.jsx
@@ -1,29 +1,42 @@
 // utils
 import { baseUrl } from "../../utils/consts";
+import { generateAuthHeader } from "../../utils/generateAuthHeader";
 
 // libraries
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function UsersPage() {
+	const [authorizing, setAuthorizing] = useState(false);
+
 	const navigate = useNavigate();
 	const [searchParams] = useSearchParams();
+
 	const code = searchParams.get("code");
 	const token = localStorage.getItem("token");
 
 	async function saveUserSpotifyAuth() {
 		try {
-			await axios.post(`${baseUrl}/auth/authorizeSpotify?code=${code}`, {
-				token,
-			});
-			navigate("/");
+			if (authorizing) {
+				await axios.post(
+					`${baseUrl}/auth/authorizeSpotify?code=${code}`,
+					{},
+					generateAuthHeader(token)
+				);
+
+				navigate("/");
+			}
 		} catch (error) {
 			console.log(error.response.data);
 		}
 	}
 	useEffect(() => {
 		saveUserSpotifyAuth();
+	}, [authorizing]);
+
+	useEffect(() => {
+		setAuthorizing(true);
 	}, []);
 
 	return <div>loading...</div>;
