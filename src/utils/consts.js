a3dc4a2915aaaadc651e44401697b2f11ae4df7c
Jenika Shannon <jenikashannon@pm.me>
2024-06-26 22:31:23 -0400

    new heroku url

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,7 +1,7 @@
-const baseUrl = "https://audiofile-api-34c71bc24fff.herokuapp.com/api";
-//"http://192.168.2.143:1700/api"
+const baseLessApi = "https://audiofile-backend-fab4f9e38733.herokuapp.com";
 
-const baseLessApi = "https://audiofile-api-34c71bc24fff.herokuapp.com";
+const baseUrl = `${baseLessApi}/api`;
+//"http://192.168.2.143:1700/api"
 
 const defaultCrateImage =
 	"https://audiofile-api-34c71bc24fff.herokuapp.com/images/crate.svg";
